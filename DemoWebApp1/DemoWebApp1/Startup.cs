6863dac4c73e7a23653e1cf7c0039f0f43b1f623
snagannagari <snagannagari@csc.com>
2016-05-19 11:11:52 +0530

    Initial Code commit

---
+++ DemoWebApp1/DemoWebApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebApp1.Startup))]
+namespace DemoWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
